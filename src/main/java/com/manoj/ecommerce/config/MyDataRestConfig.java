e58d7d4dfb70b3950eceb5c90947b14e50491759
manojpannala <manojpannala@outlook.com>
2021-09-09 20:29:30 +0300

    RestConfig

--- src/main/java/com/manoj/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/manoj/ecommerce/config/MyDataRestConfig.java
@@ -25,6 +25,5 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
-
     }
 }
